c6518b8dee1dacc956c4f7c06b36be72836be7a4
Sahil Bajaj <sahilbajaj82@gmail.com>
2020-07-06 11:21:46 +0530

    Update ctc_beam_search_decoder.cpp

--- native_client/ctcdecode/ctc_beam_search_decoder.cpp
+++ native_client/ctcdecode/ctc_beam_search_decoder.cpp
@@ -182,9 +182,8 @@ DecoderState::decode(size_t num_results) const
   // score the last word of each prefix that doesn't end with space
   if (ext_scorer_) {
     for (size_t i = 0; i < beam_size_ && i < prefixes_copy.size(); ++i) {
-      PathTrie* prefix = prefixes_copy[i];
-      PathTrie* prefix_boundary = ext_scorer_->is_utf8_mode() ? prefix : prefix->parent;
-      if (prefix_boundary && !ext_scorer_->is_scoring_boundary(prefix_boundary, prefix->character)) {
+      auto prefix = prefixes_copy[i];
+      if (!ext_scorer_->is_scoring_boundary(prefix->parent, prefix->character)) {
         float score = 0.0;
         std::vector<std::string> ngram = ext_scorer_->make_ngram(prefix);
         bool bos = ngram.size() < ext_scorer_->get_max_order();
